632f54e321643007897e67144f98c18f7bf1e4f7
Bilal Amin <bilalamin68@gmail.com>
2020-10-10 16:39:36 +0500

    url validate

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ const { get } = require("./validator");
 const Joi = require("joi");
 const middleware = require("../middlewares/middleware");
 
-/* GET home page. */
-// router.get("/", function (req, res, next) {
-//   res.json({ title: "Express" })
-// })
 
 router.get("/", middleware(get.joiSchema), controller.get);
 
--- routes/validator.js
+++ routes/validator.js
@@ -5,7 +5,7 @@ const get = {
   path: "/",
   type: "get",
   joiSchema: Joi.object({
-    url: Joi.string().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/),
+    url: Joi.string().regex(/[http,https]:\/\/[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+/),
   }),
 };
 
